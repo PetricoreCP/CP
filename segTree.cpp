f56e7c18a410f80d4a0eb2926855792514b75f24
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2025-03-17 13:08:29 +0100

    Segment Tree

--- segTree.cpp
+++ segTree.cpp
@@ -8,17 +8,17 @@ struct SegmentTree {
     vector<T> tree;
     SegmentTree(const vector<T>& v) {
         while (N < v.size()) N <<= 1;
-        // Default value
+        // Default value 0
         tree.resize(N << 1, 0);
         for (int i = 0; i < v.size(); i ++) {
             tree[N + i] = v[i];
         } 
-        // Build the tree
+        // Build the tree using query operation
         for (int i = N - 1; i > 0; i --) {
             tree[i] = tree[i << 1] + tree[(i << 1) + 1];
         }
     }
-    // Query: [l, r] 0-indexed
+    // query: [l, r] 0-indexed
     T query(int l, int r) {
         l += N;
         r += N;
@@ -37,11 +37,12 @@ struct SegmentTree {
         }
         return res;
     }
-    // Update: tree[i] to x 0-indexed
+    // update: tree[i] to x 0-indexed
     void update(int i, T x) {
         i += N;
         tree[i] = x;
-        while (i >> 1 > 0) {
+        // Update the parents using query operation
+        while ((i >> 1) > 0) {
             i >>= 1;
             tree[i] = tree[i << 1] + tree[(i << 1) + 1];
         }
