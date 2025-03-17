1fda7b2f4be7cd01e602cf357728d1b7c9b76caf
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2025-03-17 13:01:35 +0100

    Binary Modular Exponentiation

--- exp.cpp
+++ exp.cpp
@@ -1,14 +1,19 @@
-long long binModExp(long long base, long long exp, long long mod) {
-	long long ans = 1;
-	base %= mod;
-	while (exp > 0) {
-		if (exp & 1) ans = ans * base % mod;
-		base = base * base % mod; 
-		exp >>= 1;
-	}
-	return ans;
+// Binary Modular Exponentiation
+template<typename T>
+T binModExp(T base, T exp, T mod) {
+    T ans = 1;
+    base %= mod;
+    while (exp > 0) {
+        if (exp & 1) ans = ans * base % mod;
+        base = base * base % mod; 
+        exp >>= 1;
+    }
+    return ans;
 }
-
-long long binModInv(long long base, long long mod) {
+// Binary Modular Inverse
+// mod is a prime number
+// base % mod != 0
+template<typename T>
+T binModInv(T base, T mod) {
     return binModExp(base, mod - 2, mod);
 }
